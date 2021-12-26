eb91d59302c1356d5a480584ea37195a5e03c8fa
Tim <itstim1234@googlemail.com>
2021-12-26 20:45:50 +0000

    init files

---
+++ serverstart.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+killall screen
+screen -S Personal -d -m
+screen -S Personal -X stuff $'cd ~/website/Personal-Site/^M' &&
+screen -S Personal -X stuff $'bundle exec jekyll build^M'&&
+screen -S Personal -X stuff $'bundle exec jekyll serve^M'&
+#MOFNews
+screen -S MOFNews -d -m
+screen -S MOFNews -X stuff $'cd ~/website/MOFNews/^M' &&
+screen -S MOFNews -X stuff $'bundle exec jekyll build^M'&&
+screen -S MOFNews -X stuff $'bundle exec jekyll serve^M'&
+#emacsforsci
+screen -S emacsforsci -d -m
+screen -S emacsforsci -X stuff $'cd ~/website/emacsforscience^M' &&
+screen -S emacsforsci -X stuff $'bundle exec jekyll build^M'&&
+screen -S emacsforsci -X stuff $'bundle exec jekyll serve^M'&
+#TJE
+screen -S TJE -d -m
+screen -S TJE -X stuff $'cd ~/website/TJE/^M' &&
+screen -S TJE -X stuff $'bundle exec jekyll build^M'&&
+screen -S TJE -X stuff $'bundle exec jekyll serve^M'&
+sleep 30
+printf "Done! \n"
+
